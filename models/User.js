85b72aafa915a3bff041b09c4d1b83b04d31a0ca
Alisher <alisherkamal404@gmail.com>
2025-04-25 20:34:39 +0500

    remove timestamps

--- models/User.js
+++ models/User.js
@@ -4,6 +4,6 @@ const userSchema = new mongoose.Schema({
   nickname: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
-}, { timestamps: true });
+});
 
 export default mongoose.model("User", userSchema);
